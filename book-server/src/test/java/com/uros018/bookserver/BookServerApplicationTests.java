89521883518489c93bf326c5dac7545601f53203
uros018ni <uros018ni@gmail.com>
2021-02-24 16:05:09 +0100

    no message

---
+++ book-server/src/test/java/com/uros018/bookserver/BookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uros018.bookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
